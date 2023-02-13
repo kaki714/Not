3e8bb7e05c33e5620b2b08ed0e42469b04b029c8
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-13 16:09:42 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,4 @@ def run():
     print('#####  #  #  ###### ')
     print('###### ### ######## ')
     print('[º] Platform info [º]')
-    print("Name of the operating system:",os.name)
-    print("\nName of the OS system:",platform.system())
-    print("\nVersion of the operating system:",platform.release())
+    
